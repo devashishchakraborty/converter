235cb626a1546f14de598263ad0e7fa3a2af721c
Devashish Chakraborty <devashishchakra@gmail.com>
2022-12-10 23:59:58 +0530

    Created a Mile to Km Converter

---
+++ main.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+# from tkinter import ttk
+import ttkbootstrap as ttk
+
+def convert():
+    mile_input = entry_int.get()
+    km_output = mile_input * 1.61
+    output_string.set(km_output)
+
+# Window
+window = ttk.Window(themename='darkly')
+window.title("Demo")
+window.geometry("400x200")
+
+# Title
+title_label = ttk.Label(master=window, text='Miles to Kilometeres', font=("Calibri", 24, "bold"))
+title_label.pack()
+
+# Input Field
+input_frame = ttk.Frame(master=window)
+entry_int = tk.IntVar()
+
+entry = ttk.Entry(master=input_frame, textvariable=entry_int)
+button = ttk.Button(master=input_frame, text="Convert", command=convert)
+
+entry.pack(side='left', padx=10)
+button.pack(side="left")
+input_frame.pack(pady=10)
+
+# Output
+output_string = tk.StringVar()
+output_label = ttk.Label(
+    master=window, 
+    text="Output", 
+    font="Calibri 24", 
+    textvariable=output_string)
+output_label.pack()
+
+# Run
+window.mainloop()
